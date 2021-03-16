ad1e3bdc2471d6b4ef01056cb1392810e91913ad
RAVENxBLACK1 <55046858+RAVENxBLACK1@users.noreply.github.com>
2021-03-16 14:58:47 +0100

    just updated a notes.txt file

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("Netsujou no spectrum!!!");
+(()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")})),alert("I EXIST")})();
