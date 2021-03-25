15eac977d98d49d32743577b1a1a6e6e0a5e61ec
RAVENxBLACK1 <55046858+RAVENxBLACK1@users.noreply.github.com>
2021-03-25 23:37:17 +0100

    Added an api and affiched the data from the api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,20 @@
+
+
 export function handleSubmit(event) {
     event.preventDefault()
+
+    const endPoint = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+    const unit = 'units=metric';
+    const APIkey = '90abf74758a800d3b4d2dfcaa7ca08bc';
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(`${endPoint}10001&${unit}&appid=${APIkey}`)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.main.temp
     })
 }
